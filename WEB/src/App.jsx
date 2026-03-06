c0cbbfbe2f371e802336421f8e119002e0c1fe32
AmadorFiol <amfibo24@gmail.com>
2026-03-06 09:32:50 +0100

    Mod Ej Get y Add Ej Post, Put, Del

--- WEB/src/App.jsx
+++ WEB/src/App.jsx
@@ -2,14 +2,17 @@ import { useEffect } from 'react';
 import { useState } from "react";
 import axios from 'axios'
 
-function App() {
+export default function App() {
 
-    const [data, setData] = useState([{info:"Default"}]);
+    const [data, setData] = useState({info:"Default"});
 
-    useEffect(() => {
+    // Ejemplo GET
+/*    useEffect(() => {
         const url="http://127.0.0.1:8080/api/clientes"
-        axios.get(url)
+        const cif = "/TestWeb2"
+        axios.get(url+cif)
             .then(res => {
+                console.log("ResData:",res.data)
                 setData(res.data)
             })
             .catch(error => {
@@ -17,14 +20,75 @@ function App() {
             });
     }, []);
 
+    return(
+        <>
+            <p>{data.length}</p>
+            {data.length>1 ?
+                <ol>
+                    {data.map(line => (
+                        <li key={line.cif}>{line.nombre}</li>
+                    ))}
+                </ol> :
+                <pre>{JSON.stringify(data, null, 2)}</pre>}
+        </>
+    );*/
+
+    //Ejemplo POST
+/*    useEffect(() => {
+        const url="http://127.0.0.1:8080/api/clientes"
+        axios.post(url,{"cif":"TestWeb2","nombre":"TestPortalWebPost"})
+            .then(res => {
+                console.log("ResData:",res.data)
+                setData(res.data)
+            })
+            .catch(error => {
+                console.error(error.toJSON());
+            });
+    }, []);
 
     return (
-        <ol>
-            {data.map(line => (
-            <li key={line.cif}>{line.nombre}</li>
-            ))}
-        </ol>
-    );
-}
+        <>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        </>
+    );*/
 
-export default App
+    //Ejemplo PUT
+/*    useEffect(() => {
+        const url="http://127.0.0.1:8080/api/clientes"
+        const cif = "/TestWeb2"
+        axios.put(url+cif,{"nombre":"CambioConPut"})
+            .then(res => {
+                console.log("ResData:",res.data)
+                setData(res.data)
+            })
+            .catch(error => {
+                console.error(error.toJSON());
+            });
+    }, []);
+
+    return (
+        <>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        </>
+    );*/
+
+    //Ejemplo Delete
+/*    useEffect(() => {
+        const url="http://127.0.0.1:8080/api/clientes"
+        const cif = "/TestWeb1"
+        axios.delete(url+cif)
+            .then(res => {
+                console.log("ResData:",res.status)
+                setData(res.status)
+            })
+            .catch(error => {
+                console.error(error.toJSON());
+            });
+    }, []);
+
+    return (
+        <>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        </>
+    );*/
+}
