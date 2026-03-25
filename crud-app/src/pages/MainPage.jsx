3aa4d6a76702e988d66e777f067a3528b460726d
AmadorFiol <amfibo24@gmail.com>
2026-03-25 10:17:38 +0100

    Ahora la navbar es dinámica

--- crud-app/src/pages/MainPage.jsx
+++ crud-app/src/pages/MainPage.jsx
@@ -1,39 +1,45 @@
-import {useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {getPagesComponent} from "../utils/getPagesComponent.js";
-
-const pagesName = [ //Esto se cambiara por el getByUsuario
-    "clientes",
-    "etiquetas",
-    "inventarios",
-    "lotes",
-    "usuarios",
-    "paginas",
-    "roles"
-]
-
-let PAGES = [];
-
-pagesName.map(pageName=> PAGES.push({
-    key: pageName,
-    component: getPagesComponent(pageName)
-}))
-
+import {paginasApi} from "../services/api.js";
+import {toast} from "react-toastify";
+import {UserContext} from "../App.jsx";
 
 export default function MainPage(){
     const [activePage, setActivePage] = useState('')
-    const ActiveComponent = PAGES.find((p) => p.key === activePage)?.component
+    const [navbar, setNavbar] = useState([])
+    const user=useContext(UserContext)
+
+    const load = async () => {
+        try {
+            let pages= []
+            const res = await paginasApi.getByRol(user.rol.id)
+            res.data.map((p) =>
+                pages.push(
+                    {key: p.id, nombre: p.nombre, component: getPagesComponent(p.id)}
+                )
+            )
+            setNavbar(pages)
+        } catch (e) {
+            console.error(e.message)
+            toast.error(`Error al cargar la navbar: ${e.message}`)
+        }
+    }
+
+    useEffect(() => { load() }, [])
+
+    const ActiveComponent = navbar.find((p) => p.key === activePage)?.component
 
     return(
         <>
             <nav className="navbar">
                 <span className="navbar-brand">CRUD-App</span>
-                {PAGES.map((p) => (
+                {navbar.map((p) => (
                     <button
                         key={p.key}
                         className={`nav-btn${activePage === p.key ? ' active' : ''}`}
                         onClick={() => setActivePage(p.key)}
                     >
-                        {p.component.name}
+                        {p.nombre}
                     </button>
                 ))}
             </nav>
